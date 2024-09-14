839d426595c3b68e96726823dc376da90b4ae7d1
ainochi-kor <kthrkdals@gmail.com>
2024-09-14 14:19:23 +0900

    chore: typescript

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+import "ts-node/register"; // Add this to import TypeScript files
+import { ExpoConfig } from "expo/config";
+
+const config: ExpoConfig = {
+  name: "stickersmash",
+  slug: "stickersmash",
+};
+
+export default config;
